6b5cc1ebe510828e0e3d69fbfe9d3974c652a47c
Harsh <harshbhama96@gmail.com>
2019-03-24 17:42:45 +0530

    add func components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import React from "react"
-import ReactDOM from "react-dom" 
+import ReactDOM from "react-dom" //To render html into the page.
 
 // ReactDOM.render(<div><h1>Hello</h1><p>This is paragraph</p></div>, document.getElementById("root"))
 
-ReactDOM.render(<ul>
-	<li>1.Hello</li>
-	<li>2.Yes</li>
-	<li>3.OK</li>
-	</ul>, document.getElementById("root"))
+// ReactDOM.render(<ul>
+// 	<li>1.Hello</li>
+// 	<li>2.Yes</li>
+// 	<li>3.OK</li>
+// 	</ul>, document.getElementById("root"))
+
+// function MyApp(){
+
+// 	return (
+
+// 		<ul>
+// 		 	<li>1.Hello</li>
+// 		 	<li>2.Yes</li>
+// 		 	<li>3.NO</li>
+// 		</ul>
+
+
+// 		)
+// }
+
+function MyInfo(){
+
+	return (
+
+	<div>	
+		<h1 class = "head"> Harsh </h1>
+		<p> Hello there !! </p>
+		<ul>
+			<li> 1.yes </li>
+			<li> 2.NO </li>
+
+		</ul>
+	</div>
+
+	)
+
+}
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"))
