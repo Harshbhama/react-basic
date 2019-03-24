bba06945d90af14e81c0236af172518391d46ea2
Harsh <harshbhama96@gmail.com>
2019-03-24 14:25:26 +0530

    add ul tags-DOM render

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom" 
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"))
+// ReactDOM.render(<div><h1>Hello</h1><p>This is paragraph</p></div>, document.getElementById("root"))
+
+ReactDOM.render(<ul>
+	<li>1.Hello</li>
+	<li>2.Yes</li>
+	<li>3.OK</li>
+	</ul>, document.getElementById("root"))
