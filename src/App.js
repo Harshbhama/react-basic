1403d79d51d9521b55236be11bffae4746def279
Harsh <harshbhama96@gmail.com>
2019-03-25 22:46:11 +0530

    jsx to javascript

--- src/App.js
+++ src/App.js
@@ -5,12 +5,32 @@ import MainContent from "./components/MainContent"
 
 
 function App(){
+	const fname = "harsh"
+	const lname = "bhama"
+
+	const date = new Date()
+	const hour = date.getHours()
+
+
+	let time
+
+	if(hour<12){
+		time = "Good morning"
+	}
+	else if(hour>=12 &&hour<17)
+	{
+		time = "Good afternoon"
+	}
+	else{
+		time = "Good night"
+	}
+
 	return(
 		<div>
 
-			<Header />
-			<MainContent />
-			<Footer />
+			<h1> Hello {fname+" "+lname}</h1>
+			<h3>{time} {hour}</h3>
+
 
 		</div>
 		)
