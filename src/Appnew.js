775de0efa855458edcdc35a3fa3970d63cac5520
Harsh <harshbhama96@gmail.com>
2019-03-31 13:09:53 +0530

    class comp practice

--- src/Appnew.js
+++ src/Appnew.js
@@ -1,16 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-// function Appnew(){
-//   return(
-//
-//     <div>
-//         <h1>Hey there !!</h1>
-//     </div>
-//
-//   )
-// }
-//
 
 class Appnew extends React.Component{
 
@@ -18,11 +8,55 @@ class Appnew extends React.Component{
         return(
 
           <div>
-              <h1>Hey there !!</h1>
+              <Header
+              username = "harsh"
+              />
+              <Greetings />
+
           </div>
 
         )
     }
 }
+
+class Header extends React.Component{
+
+  render(){
+      return(
+
+          <div>
+              <p>Welcome, {this.props.username}! </p>
+          </div>
+
+      )
+
+  }
+}
+class Greetings extends React.Component{
+  render(){
+      const date = new Date();
+      const hours = date.getHours();
+      let time
+      if (hours>12 && hours <17){
+        time = "Good afternoon"
+      }
+      else if (hours<12){
+        time = "Good morning"
+      }
+      else{
+        time = "Good evening"
+      }
+      return(
+
+        <div>
+          <h1>{time} to you sir or madam</h1>
+        </div>
+      )
+  }
+}
+
+
+
+
 
 export default Appnew
